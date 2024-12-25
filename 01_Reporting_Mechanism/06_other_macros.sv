e6b3396b7fc54633133638f2098c68d396dad809
charankumarreddychintha@gmail.com <charankumarreddychintha@Charans-MacBook-Air.local>
2024-12-25 15:36:59 -0500

    other macros and error count

--- 01_Reporting_Mechanism/06_other_macros.sv
+++ 01_Reporting_Mechanism/06_other_macros.sv
@@ -16,7 +16,9 @@ class driver extends uvm_driver;
         `uvm_warning("DRV", "potential Error");
         `uvm_error("DRV", "Real Error");
         #10;
-        `uvm_fatal("DRV", "simulation cannot continue");
+        `uvm_fatal("DRV", "simulation cannot continue for DRV");
+        #10;
+        `uvm_fatal("DRV1", "simulation cannot continue for DRV1");
     endtask
 endclass
 
@@ -25,6 +27,9 @@ module tb;
 
     initial begin
         d = new("DRV", null);
+        //severity override from UVM_FATAL to UVM_ERROR
+        d.set_report_severity_override(UVM_FATAL, UVM_ERROR);
+        d.set_report_severity_id_override(UVM_FATAL, "DRV1", UVM_ERROR);
         d.run();
     end
 endmodule
